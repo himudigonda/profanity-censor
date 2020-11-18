fd7dfefe70ea5c82aeb96933f7d08fbfadcc1f59
mudigonda <himansh02@hotmail.com>
2020-11-18 17:50:58 +0530

    Update run.java

--- run.java
+++ run.java
@@ -9,7 +9,7 @@ import java.io.FileReader;
 public class run extends FilterClass {
     public static void main(String[] args) throws Exception {
         try {
-            FileReader fr = new FileReader("/home/mudigonda/Documents/SRM/Semester-3/OOPS/Assignments/Project/Lyrics.txt");
+            FileReader fr = new FileReader("/home/hhimmmmii/Documents/Project/Lyrics.txt");
             BufferedReader input = new BufferedReader(fr);
             String currentLine = "";
             while ((currentLine = input.readLine()) != null) {
